422520e7ce2785d10a24f29a2861c8405fbae88c
noobjc <noobjc@localhost>
2021-07-06 05:32:43 -0600

    working scatter/gather io

--- main.cc
+++ main.cc
@@ -5,39 +5,51 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/uio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
-int main() {
+int main(int argc, char **argv) {
+    struct sockaddr_in server_addr;
+
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(fd < 0){
+        printf("error: unable to create socket\n");
+        return -1;
+    }
+    printf("info: socket created successfully\n");
+    
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(2000);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    
+    if(connect(fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+        printf("error: unable to connect\n");
+        return -1;
+    }
+    printf("info: connected with server successfully\n");
+    
     struct iovec iov[3];
-    ssize_t nr;
-    int fd, i;
-
     char *buf[] = {
-        "Hello world!",
-        "This is a test",
-        "Of the BSD syscall"
+        "Hello",
+        "This",
+        "Ofc"
     };
 
-    fd = open("buccaneer.txt", O_WRONLY);
-    if (fd == -1) {
-        perror("open failed");
-        return 1;
-    }
-
-    for (i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++) {
         iov[i].iov_base = buf[i];
         iov[i].iov_len = strlen(buf[i]);
     }
 
-    nr = writev (fd, iov, 3);
+    ssize_t nr = writev (fd, iov, 3);
     if (nr == -1) {
-        perror ("writev failed");
+        perror("error: call to writev failed");
         return 1;
     }
 
-    printf ("wrote %ld bytes\n", nr);
+    printf ("info: wrote %ld bytes\n", nr);
 
     if (close(fd)) {
-        perror ("close");
+        perror("info: close");
         return 1;
     }
 }
--- server.cc
+++ server.cc
@@ -2,72 +2,73 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/uio.h>
+
 
 int main(int argc, char** argv) {
-    int socket_desc, client_sock, client_size;
+    int socket_desc, client_sock;
+    socklen_t client_size;
     struct sockaddr_in server_addr, client_addr;
-    char server_message[100], client_message[100];
-    
-    // Clean buffers:
-    memset(server_message, '\0', sizeof(server_message));
-    memset(client_message, '\0', sizeof(client_message));
-    
-    // Create socket:
+
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
     
     if(socket_desc < 0){
-        printf("Error while creating socket\n");
+        printf("error: error while creating socket\n");
         return -1;
     }
-    printf("Socket created successfully\n");
+    printf("info: socket created successfully\n");
     
-    // Set port and IP:
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(2000);
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    
-    // Bind to the set port and IP:
     if(bind(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr))<0){
-        printf("Couldn't bind to the port\n");
+        printf("error: couldn't bind to the port\n");
         return -1;
     }
-    printf("Done with binding\n");
+    printf("info: done with binding\n");
     
-    // Listen for clients:
     if(listen(socket_desc, 1) < 0){
-        printf("Error while listening\n");
+        printf("error: error while listening\n");
         return -1;
     }
-    printf("\nListening for incoming connections.....\n");
+    printf("info: listening for incoming connections.....\n");
     
-    // Accept an incoming connection:
-    client_size = sizeof(client_addr);
-    client_sock = accept(socket_desc, (struct sockaddr*)&client_addr, &client_size);
-    
-    if (client_sock < 0){
-        printf("Can't accept\n");
-        return -1;
+    while (1) {
+        char *msg_a = new char[5];
+        char *msg_b = new char[4];
+        char *msg_c = new char[3];
+        struct iovec iov[3];
+        ssize_t nr;
+
+        iov[0].iov_base = msg_a;
+        iov[0].iov_len = 5;
+        iov[1].iov_base = msg_b;
+        iov[1].iov_len = 4;
+        iov[2].iov_base = msg_c;
+        iov[2].iov_len = 3;
+
+        client_size = sizeof(client_addr);
+        client_sock = accept(socket_desc, (struct sockaddr*)&client_addr, &client_size);
+        
+        if (client_sock < 0){
+            printf("error: can't accept\n");
+            return -1;
+        }
+        printf("info: client connected at IP: %s and port: %i\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+        nr = readv(client_sock, iov, 3);
+        if (nr == -1) {
+            perror("error: readv failed");
+            return 1;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            printf ("\n%d: %s\n", i, (char *) iov[i].iov_base);
+        }
     }
-    printf("Client connected at IP: %s and port: %i\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
     
-    // Receive client's message:
-    if (recv(client_sock, client_message, sizeof(client_message), 0) < 0){
-        printf("Couldn't receive\n");
-        return -1;
-    }
-    printf("Msg from client: %s\n", client_message);
-    
-    // Respond to client:
-    strcpy(server_message, "This is the server's message.");
-    
-    if (send(client_sock, server_message, strlen(server_message), 0) < 0){
-        printf("Can't send\n");
-        return -1;
-    }
-    
-    // Closing the socket:
     close(client_sock);
     close(socket_desc);
-    
     return 0;
 }
